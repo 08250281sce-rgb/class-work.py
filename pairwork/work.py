eff46881861bd4c744ae1dae2075bea710335b56
unknown <08250281.sce@rub.edu.bt>
2026-03-27 16:32:31 +0600

    adding if function

--- pairwork/work.py
+++ pairwork/work.py
@@ -3,4 +3,10 @@ m1 = float(input("input your dzo marks:"))
 m2 = float(input("input your eng marks:"))
 m3 = float (input("input your math marks:"))
 m4 = float(input("input your science marks:"))
+ 
+if m1 >= 60 and m2 >= 60 and m3 >= 60 and m4 >= 60:
+    print("congratulations", name, "you have passed the exam")
+else:
+    
 
+       
