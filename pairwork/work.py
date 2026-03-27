05dee1ab68f17cde4f6dbd55cf1786b276c8632b
unknown <08250281.sce@rub.edu.bt>
2026-03-27 16:23:43 +0600

    adding marks

--- pairwork/work.py
+++ pairwork/work.py
@@ -1,2 +1,6 @@
 name = input("enter your name :")
 m1 = float(input("input your dzo marks:"))
+m2 = float(input("input your eng marks:"))
+m3 = float (input("input your math marks:"))
+m4 = float(input("input your science marks:"))
+
