efd63c68f1c0bdb821f8f47acd5bc30dec08ca81
unknown <08250264.sce@rub.edu.bt>
2026-03-27 23:53:21 +0600

    pair work for pyhton

--- pairwork/work.py
+++ pairwork/work.py
@@ -1,19 +1,33 @@
-name = input("enter your name :")
-m1 = float(input("input your dzo marks:"))
-m2 = float(input("input your eng marks:"))
-m3 = float (input("input your math marks:"))
-m4 = float(input("input your science marks:"))
-average = (m1+m2+m3+m4)/4
- 
-if m1 >= 60 and m2 >= 60 and m3 >= 60 and m4 >= 60:
-    print("congratulations", name, "you have passed the exam")
+name =(input ("enter name:"));
+print(name)
+module_1= float(input("enter mark1:"));
+module_2= float(input("enter mark2:"));
+module_3= float(input("enter mark3:"));
+module_4= float(input("enter mark4:"));
+total_mark= module_1 + module_2 + module_3 + module_4
+average = total_mark/4
+print ("The average of your marks is ""%.2f" % (average))
+attendance = float (input("enter your total attendance:"));
+if (attendance >70):
+    print("you are eligible for the exam")
 else:
-    
+    print("you are short of attendance, so not eligible to write exam.") 
+if (average >40):
+    print(" congargulation! you have passed the exam")
+else:
+    print(" Therfore you have failed")           
 
-# <<<<<<< Updated upstream
-       
-# =======
-
-
-# >>>>>>> Stashed change
-
+if (average >= 90 ):
+    print("Grade A")
+elif(average >= 80 ):
+    print("Grade B")
+elif (average >= 70):
+    print("Grade C")
+elif (average >= 60):
+    print("Grade D")
+elif (average >= 50):
+    print("Grade E")  
+if (average >90):
+    print("eligible for the reward")
+else:
+    print("reward is not posiible this time,try next semester.")
