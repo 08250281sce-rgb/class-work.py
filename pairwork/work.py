a942678cc70a64f55705165d0f209605a0df1969
unknown <08250264.sce@rub.edu.bt>
2026-03-27 23:50:08 +0600

    pair work for python

--- pairwork/work.py
+++ pairwork/work.py
@@ -3,15 +3,17 @@ m1 = float(input("input your dzo marks:"))
 m2 = float(input("input your eng marks:"))
 m3 = float (input("input your math marks:"))
 m4 = float(input("input your science marks:"))
+average = (m1+m2+m3+m4)/4
  
 if m1 >= 60 and m2 >= 60 and m3 >= 60 and m4 >= 60:
     print("congratulations", name, "you have passed the exam")
 else:
     
 
-<<<<<<< Updated upstream
+# <<<<<<< Updated upstream
        
-=======
-average = (m1+m2+m3+m4)/4
+# =======
 
->>>>>>> Stashed changes
+
+# >>>>>>> Stashed change
+
